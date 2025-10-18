bc57371f13d9bc19db85351e4f109b3e85b6b3dd
cherifloua <loua.cherif@esprit.tn>
2025-10-18 12:00:29 -0700

    Add my project files

---
+++ centre.h
@@ -0,0 +1,26 @@
+#ifndef CENTRE_H
+#define CENTRE_H
+#define MAX_NOM 100
+#define MAX_ADRESSE 200
+#define MAX_VILLE 50
+#define MAX_TELEPHONE 20
+#define MAX_EMAIL 100
+typedef struct {
+  int id_centre;
+  char nom[MAX_NOM];
+  char adresse[MAX_ADRESSE];
+  char ville[MAX_VILLE];
+  int capacite;
+  char telephone[MAX_TELEPHONE];
+  char email[MAX_EMAIL];
+  int actif;
+}Centre;
+void ajouter_centre(Centre *nouveau_centre);
+void modifier_centre(int id_centre,Centre *centre_modifie);
+void supprimer_centre(int id_centre);
+Centre* rechercher_centre_par_id(int id_centre);
+centre* rechercher_centre_par_nom(const char *nom);
+void afficher_tous_centres();
+int obtenir_nombre_centres();
+#endif
+
---
+++ src/callbacks.h
@@ -0,0 +1,138 @@
+#include <gtk/gtk.h>
+
+
+void
+on_button_ajouter_clicked              (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_modifier_clicked             (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_supprimer_clicked            (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_rechercher_clicked           (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_s_inscrire_clicked           (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_connexion_clicked            (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_check_admin_clicked                 (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_gestion_entraineur_clicked   (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_gestion_membre_enter         (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_gestion_centre_clicked       (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_gestion_cours_clicked        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_gestion_equipement_clicked   (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_spinbutton_capacite_ajout_changed   (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_spinbutton_id_ajout_delete_text     (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_radiobutton_inactif_ajout_clicked   (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_radiobutton_actif_ajout_clicked     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_annuler_ajout_clicked        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_valider_ajout_clicked        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_spinbutton_id_modif_change_value    (GtkEditable     *editable,
+                                        gpointer         user_data);
+
+void
+on_radiobutton_actif_modif_clicked     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_radiobutton_inactif_modif_clicked   (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_modifier_centre_clicked      (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_annuler_modif_clicked        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_oui_supprimer_clicked        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_non_supprimer_clicked        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_radiobutton_recherche_ville_clicked (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_radiobutton_recherche_nom_clicked   (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_annuler_recherche_clicked    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_lancer_recherche_clicked     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_valider_inscription_clicked  (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_annuler_inscription_clicked  (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_administrateur_clicked       (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_entraineur_clicked           (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_connexion_entraineur_clicked (GtkButton       *button,
+                                        gpointer         user_data);
